2103b20849a2a8f964305a7a580b6efaaa31dd2a
daisymai99 <hongngoc1820@gmail.com>
2021-11-16 18:15:55 +0700

    layout sms

--- app/src/main/java/com/little_bird/movieapp/MainActivity.java
+++ app/src/main/java/com/little_bird/movieapp/MainActivity.java
@@ -2,13 +2,55 @@ package com.little_bird.movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtNumber;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView yxyy = findViewById(R.id.txt);
+        callPopUpSignIn();
     }
+
+    private void callPopUpSignIn (){
+        Dialog mdialog = new Dialog(this);
+        mdialog.setContentView(R.layout.pop_up_sign_in);
+        Button btnContinue = findViewById(R.id.btnContinue);
+
+        edtNumber = findViewById(R.id.edtNumber);
+        mdialog.show();
+
+        ImageView img = findViewById(R.id.btnClose);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Sms.class));
+            }
+        });
+
+    }
+
+
 }
