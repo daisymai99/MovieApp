735f5148426d19def5eff25b5541779e7c6ef3b4
daisymai99 <hongngoc1820@gmail.com>
2021-11-26 16:52:57 +0700

    navigation bottom & viewpager2

--- app/src/main/java/com/little_bird/movieapp/MainActivity.java
+++ app/src/main/java/com/little_bird/movieapp/MainActivity.java
@@ -1,26 +1,40 @@
 package com.little_bird.movieapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.little_bird.movieapp.fragments.FragmentAdapter;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager2 viewPager2;
+    FragmentAdapter fragmentAdapter;
+    TabLayout tableLayout;
 
-
+    BottomNavigationView bottomNavigation;
 
 
     @Override
@@ -28,8 +42,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         callPopUpSignIn();
+
+        addBottomNav();
+        addViewPager2();
     }
 
     private void callPopUpSignIn (){
@@ -47,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                mdialog.dismiss();
             }
         });
 //
@@ -68,6 +87,77 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 10000);
     }
+
+
+    private void addViewPager2(){
+        tableLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fragmentAdapter = new FragmentAdapter(fm,getLifecycle());
+
+        tableLayout.addTab(tableLayout.newTab().setText("Anime"));
+        tableLayout.addTab(tableLayout.newTab().setText("BXH"));
+
+        tableLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tableLayout.selectTab(tableLayout.getTabAt(position));
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+        });
+    }
+
+    private void addBottomNav(){
+        bottomNavigation = findViewById(R.id.bottom_nav);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        MainActivity.super.onBackPressed();
+                        return true;
+                    case R.id.user:
+                        new Intent(getApplicationContext(),SetPassword.class);
+                        return true;
+
+                    case R.id.settings:
+
+                        new Intent(getApplicationContext(),Sms.class);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
 
 
 }
